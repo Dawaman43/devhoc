bc3969eb2868b19784c3a26b5fcc52837c1bfcd0
Dawaman43 <dawitworkujima@gmail.com>
2025-12-06 00:06:24 +0300

    feat: update Home component to include Hero, TrendingPosts, and SolvedProblems

--- frontend/src/components/home/Home.tsx
+++ frontend/src/components/home/Home.tsx
@@ -1,3 +1,13 @@
+import Hero from './Hero'
+import SolvedProblems from './Solved'
+import TrendingPosts from './Trending'
+
 export default function Home() {
-  return <div>home</div>
+  return (
+    <div>
+      <Hero />
+      <TrendingPosts />
+      <SolvedProblems />
+    </div>
+  )
 }
